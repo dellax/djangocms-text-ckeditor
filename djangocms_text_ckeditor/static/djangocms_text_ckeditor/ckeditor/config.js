0454fc51f075f9046c974e0b91e2f7104b327cc1
MiMa <michalnebbia@gmail.com>
2015-12-19 21:07:09 +0100

    added font awesome plugin

--- djangocms_text_ckeditor/static/djangocms_text_ckeditor/ckeditor/config.js
+++ djangocms_text_ckeditor/static/djangocms_text_ckeditor/ckeditor/config.js
@@ -7,4 +7,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+		config.removeDialogTabs = 'image:advanced;link:advanced';
+	config.extraPlugins = 'widget,lineutils,FontAwesome';
+	config.contentsCss = 'css/font-awesome.css';
+	config.allowedContent = true;
+	config.fontAwesome_version = '4.5';
 };
